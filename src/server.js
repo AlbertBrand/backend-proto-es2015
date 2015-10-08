a3df2afabd345af9172a8f442f7898ff04517275
Albert Brand <abrand@xebia.com>
2015-10-08 08:30:40 +0200

    Use Map instead of object for route lookup

--- src/server.js
+++ src/server.js
@@ -4,30 +4,30 @@ const http = require('http'),
   co = require('co'),
   sleep = require('./sleep');
 
-const routeHandlers = {
-  '/async': (req, res) => {
-    co(function *() {
-      const a = yield sleep(1000, 123);
-      const b = yield sleep(2000, a + 456);
-      res.end(`asynchronous result: ${a} ${b}`);
+const routeHandlers = new Map();
 
-    }).catch(() => {
-      res.writeHead(500);
-      res.end('internal error');
-    });
-  },
+routeHandlers.set('/async', (req, res) => {
+  co(function *() {
+    const a = yield sleep(1000, 123);
+    const b = yield sleep(2000, a + 456);
+    res.end(`asynchronous result: ${a} ${b}`);
 
-  '/': (req, res) => {
-    res.writeHead(200);
-    res.end('okay');
-  }
-};
+  }).catch(() => {
+    res.writeHead(500);
+    res.end('internal error');
+  });
+});
+
+routeHandlers.set('/', (req, res) => {
+  res.writeHead(200);
+  res.end('okay');
+});
 
 const server = http.createServer((req, res) => {
-  const handler = routeHandlers[req.url];
-  if (handler) {
-    return handler(req, res);
+  if (routeHandlers.has(req.url)) {
+    return routeHandlers.get(req.url)(req, res);
   }
+
   res.writeHead(404);
   res.end('not found');
 });
